b8ac2a26b6da04c83b6d69c239e2ffda763bec9b
brutus-cfd <65898278+brutus-cfd@users.noreply.github.com>
2020-05-28 20:19:09 +0530

    upp

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "17f7ca93ede86526fed4f18f32583537",
 "/": "17f7ca93ede86526fed4f18f32583537",
-"main.dart.js": "0478ca174fa1fcaec8095fde04b14b56",
+"main.dart.js": "b9993aa446771f72fdff0210ba3213a3",
 "manifest.json": "54d7d35c63a247117ce00c942d6dd58e"
 };
 
